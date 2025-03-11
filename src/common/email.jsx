7967bfc71c8056d2e6a26778b2954a8dafa7919b
devaanantawijaya <gededeva11@gmail.com>
2025-03-11 16:04:52 +0800

    update component NewsLetter

--- src/common/email.jsx
+++ src/common/email.jsx
@@ -4,29 +4,29 @@ import Wave from "../common/assets/images/Wave.svg"; // Pastikan path ini benar
 export default function NewsLetter() {
   return (
     <React.Fragment>
-      <section className="relative bg-blue-900 text-white overflow-hidden flex justify-center items-center">
+      <section className="relative flex items-center justify-center overflow-hidden text-white bg-blue-900">
         {/* Mengganti elemen svg dengan img */}
         <img
           src={Wave}
           alt="Wave"
-          className="absolute inset-x-0 bottom-0 w-full z-20"
+          className="absolute inset-x-0 bottom-0 z-20 w-full"
         />
 
-        <div className="container mx-auto px-4 py-24 md:py-32 relative z-10 text-center">
-          <div className="w-full md:w-1/2 mx-auto mb-12 md:mb-0">
-            <h1 className="text-5xl md:text-6xl  font-bold mb-6 text-white leading-tight">
+        <div className="container relative z-10 px-4 py-24 mx-auto text-center md:py-32">
+          <div className="w-full mx-auto mb-12 md:w-1/2 md:mb-0">
+            <h1 className="mb-6 text-5xl font-bold leading-tight text-white md:text-6xl">
               Join our newsletter to get the latest guides!
             </h1>
-            <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+            <div className="flex flex-col justify-center space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
               <a
                 href="#"
-                className="bg-green-50 text-blue-900 font-semibold px-8 py-3 rounded-full hover:bg-blue-100 transition duration-300 text-center"
+                className="px-8 py-3 font-semibold text-center text-blue-900 transition duration-300 rounded-full bg-green-50 hover:bg-blue-200"
               >
                 Get Started
               </a>
               <a
                 href="#"
-                className="border-2 border-white text-white font-semibold px-8 py-3 rounded-full hover:bg-white hover:text-blue-900 transition duration-300 text-center"
+                className="px-8 py-3 font-semibold text-center text-white transition duration-300 border-2 border-white rounded-full hover:bg-white hover:text-blue-900"
               >
                 Learn More
               </a>
