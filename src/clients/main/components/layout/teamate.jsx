1e61e18eb433d3d3c4843fb1373164d6f2fb1720
devaanantawijaya <gededeva11@gmail.com>
2025-03-11 17:14:23 +0800

    update: Teamate

--- src/clients/main/components/layout/teamate.jsx
+++ src/clients/main/components/layout/teamate.jsx
@@ -13,18 +13,14 @@ export default function TeaMate() {
 
   return (
     <React.Fragment>
-      <div className="text-center">
-        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center text-blue-700">
-          Our Teammate
-        </h2>
-      </div>
+      <div className="mt-10 text-center md:mt-0"></div>
       {/* About adiHutama Section */}
-      <section className="hero bg-white pb-16 px-4 sm:px-8">
-        <div className="hero-content flex flex-col lg:flex-row items-center justify-center max-w-7xl mx-auto">
+      <section className="px-4 pb-16 bg-white hero sm:px-8">
+        <div className="flex flex-col items-center justify-center mx-auto hero-content lg:flex-row max-w-7xl">
           {/* Skeleton Loader */}
           {!imageLoaded && (
-            <div className="skeleton w-full max-w-xs lg:max-w-sm rounded-lg shadow-2xl mb-8 lg:mb-0 lg:mr-16">
-              <div className="animate-pulse bg-gray-300 h-48 lg:h-64 rounded-lg" />
+            <div className="w-full max-w-xs mb-8 rounded-lg shadow-2xl skeleton lg:max-w-sm lg:mb-0 lg:mr-16">
+              <div className="h-48 bg-gray-300 rounded-lg animate-pulse lg:h-64" />
             </div>
           )}
           {/* Actual Image */}
@@ -38,26 +34,29 @@ export default function TeaMate() {
             loading="lazy" // Lazy load the logo image
           />
           <div className="text-center lg:text-left">
-            <p className="text-lg sm:text-xl text-justify max-w-2xl">
+            <h2 className="pb-5 text-3xl font-bold text-blue-700 md:text-4xl lg:text-5xl">
+              Our Teammate
+            </h2>
+            <p className="max-w-2xl text-lg text-justify sm:text-xl">
               We move with make a Creative Strategy for help your business goal,
               we help to improve your income by a services we have. make your
               content look interesting and make people look for your business
             </p>
-            <p className="text-lg sm:text-xl text-justify max-w-2xl py-2">
+            {/* <p className="max-w-2xl py-2 text-lg text-justify sm:text-xl">
               We move with make a Creative Strategy for help your business goal,
               we help to improve your income by a services we have. make your
               content look interesting and make people look for your business
-            </p>
-            <div className="flex py-5 gap-x-5">
+            </p> */}
+            <div className="flex justify-center py-5 gap-x-5 lg:justify-normal">
               <a
                 href="#"
-                className="bg-blue-700 text-white font-semibold px-8 py-3 rounded-full hover:bg-blue-800 transition duration-300 text-center"
+                className="px-8 py-3 font-semibold text-center text-white transition duration-300 bg-blue-700 rounded-full hover:bg-blue-900"
               >
                 View More
               </a>
               <a
                 href="#"
-                className="bg-white text-blue-800 border border-blue-700 font-semibold px-8 py-3 rounded-full hover:bg-gray-100 transition duration-300 text-center"
+                className="px-8 py-3 font-semibold text-center text-blue-900 transition duration-300 bg-white border border-blue-700 rounded-full hover:bg-blue-700 hover:text-white"
               >
                 Our Story
               </a>
